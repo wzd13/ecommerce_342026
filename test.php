e6ecd4e80bb4ded74664d6007508e324c6deb55b
Ng Jun Zhe <jzljz432@gmail.com>
2026-04-03 09:48:11 +0800

    Delete test.php

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'config/config.php';
-$stmt = $pdo->query("SELECT * FROM ProductImages");
-$images = $stmt->fetchAll(PDO::FETCH_ASSOC);
-print_r($images);
-
-$stmt2 = $pdo->query("SELECT p.ProductId, p.ProductName, (SELECT ImageUrl FROM ProductImages WHERE ProductId = p.ProductId AND IsPrimary = 1 LIMIT 1) as PrimaryImage FROM Products p");
-$products = $stmt2->fetchAll(PDO::FETCH_ASSOC);
-print_r($products);
